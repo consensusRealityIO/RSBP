4a96caafb42eb06ef89740ef8c9ef1f07dfb5857
Giulio Meneghin <bluemana@gmail.com>
2016-05-19 17:22:34 +0800

    Fix RSBP_CONFIG property spelling

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ var RSBP_CONFIG = {
     "name": "consensusReality",
     "address": "1E16XPFWKY2XaFDDzS6V93hGdXP2QEnddY",
     "currency": "BTC",
-    "defualtAmount": 0,
+    "defaultAmount": 0,
     "discount": 0
   }
 };
