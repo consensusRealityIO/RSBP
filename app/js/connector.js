7423873dd5ec0ecd4ab9d4bb2598b3d81db1d082
Giulio Meneghin <bluemana@gmail.com>
2016-10-13 20:02:48 +0700

    Check connectivity via WebSocket

--- app/js/connector.js
+++ app/js/connector.js
@@ -1,47 +1,89 @@
-var RSBP = (function (RSBP, window) {
+var RSBP = (function (RSBP, window, $) {
 
   "use strict";
 
-  window.addEventListener("load", function () {
+  let online = false;
 
-    var online = function () {
+  let doOnline = function () {
+    if (!online) {
+      online = true;
       $(".status-div").addClass("invisible");
       $(".status-content").text("");
-    };
+      console.info("App online");
+    }
+  };
 
-    var offline = function () {
+  let doOffline = function () {
+    if (online) {
+      online = false;
       $(".status-div").removeClass("invisible");
       $(".status-content").text("Connection error");
+      console.info("App offline");
+    }
+  };
+
+  let setupWebSocket = function () {
+
+    let TIMEOUT = 1500;
+
+    let webSocket = new WebSocket("ws://echo.websocket.org/");
+
+    let sentTimestamp = -1;
+    let receivedTimestamp = -1;
+    let ping = null;
+    let checkConnectivity = null;
+
+    webSocket.onopen = function(evt) {
+      console.info("WebSocket opened");
+      ping = window.setInterval(function () {
+        sentTimestamp = Date.now();
+        webSocket.send("{}");
+      }, TIMEOUT);
+      checkConnectivity = window.setInterval(function () {
+        if (receivedTimestamp != -1 &&
+            Math.abs(receivedTimestamp - sentTimestamp) <= TIMEOUT) {
+          doOnline();
+        } else {
+          doOffline();
+        }
+      }, TIMEOUT);
     };
 
-    if (window.navigator.onLine) {
-      console.info("App online on load");
-      online();
-    } else {
-      console.info("App offline on load");
-      offline();
-    }
+    webSocket.onclose = function(evt) {
+      console.info("WebSocket closed");
+      window.clearInterval(ping);
+      window.clearInterval(checkConnectivity);
+      doOffline();
+      window.setTimeout(setupWebSocket, 1000);
+    };
 
-    window.addEventListener("offline", function () {
-      console.info("App offline");
-      offline();
-    });
+    webSocket.onmessage = function(evt) {
+      receivedTimestamp = Date.now();
+    };
 
-    window.addEventListener("online", function () {
-      console.info("App online");
-      online();
-    });
-  });
+    webSocket.onerror = function(evt) {
+      console.error("WebSocket error: " + JSON.stringify(evt || {}));
+      window.clearInterval(ping);
+      window.clearInterval(checkConnectivity);
+      doOffline();
+    };
+  };
 
-  var fetch = function (url) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", url, false);
-      xhr.send(null);
-      return JSON.parse(xhr.responseText);
-  }
+  let fetch = function (url, callback) {
+      let xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+          callback(xhr.responseText, xhr.status);
+        }
+      };
+      xhr.open("GET", url);
+      xhr.send();
+  };
+
+  setupWebSocket();
 
   RSBP.fetch = fetch;
 
   return RSBP;
 
-}(RSBP || {}, window));
+}(RSBP || {}, window, $));
