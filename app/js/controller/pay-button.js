513c20c16121454ecc5c142c4510c41676e8266c
Cédric Félizard <cedric@felizard.fr>
2017-01-25 12:27:15 +0000

    Display error message below amount input

--- app/js/controller/pay-button.js
+++ app/js/controller/pay-button.js
@@ -14,12 +14,17 @@
                 RSBP.rate.isValid();
 
     $("#pay-button").prop("disabled", !valid);
+
+    let input = $("#currency-amount-input-field")[0];
+    let error = input.checkValidity() ? "" : input.validationMessage;
+
+    $("#currency-amount-input-error").text(error);
   };
 
   $(document).ready(function () {
     console.info("Initializing pay button controller...");
-    update();
-    $("#currency-amount-input-field").on("input", update);
+    $("#currency-amount-input-field").on("keyup", update);
+    $("#currency-amount-input-field").on("change", update);
     window.addEventListener("connectivity", update);
     window.addEventListener("rate", update);
     console.info("Pay button controller initialized");
